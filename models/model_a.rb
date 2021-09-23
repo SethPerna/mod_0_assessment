27a362c01223d4277b5a56c65128aff62aab3dbb
Seth Perna <gseth26@gmail.com>
2021-09-23 08:18:26 -0400

    dinosaur update

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name" => "Buddy",
+  "species" => "T-rex",
+  "age" => 36,
+  "height" => 27,
+  "is mean" => true,
+  "is purple" => false,
+  "limbs" => ["arms", "legs"],
+  "senses" => ["sight", "hearing", "smell", "taste"]
+}
