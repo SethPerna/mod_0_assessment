84a9f1151eeef59fb8633adf6c8f6d3746926cd7
Seth Perna <gseth26@gmail.com>
2021-09-23 08:10:55 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Super Troopers"
+is_entertaining = true
+movie = "Beerfest"
+num_4 = 4
+num_6 = 6
+product = "#{num_4 * num_6}"
+favorite_foods = ["queso", "nachos", "tacos"]
+favorite_foods.delete_at(0)
+movie.length
