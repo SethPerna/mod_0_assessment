a3e5a911fc10e6d788b31ce13d243ac1ee23946f
Seth Perna <gseth26@gmail.com>
2021-09-23 08:23:01 -0400

    developer update

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Seth",
+  "species" => "Human?",
+  "height" => 75,
+  "age" => 29,
+  "smart" => false,
+  "kind" => true,
+  "hobbies" => ["skateboarding", "snowboarding", "hiking", "kayayking"],
+  "family" => ["Jacey", "Levon", "Dad", "Brother"]
+}
